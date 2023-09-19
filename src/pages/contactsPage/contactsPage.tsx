d8f91747d41636c3d9b640a1592bbbbdeaf46bc0
@KsuSmolyar <ibragimova-ksenija@lenta.ru>
2023-09-19 10:54:08 +0400

    stylization aboutPage, deliveryPage, contactsPage

--- src/pages/contactsPage/contactsPage.tsx
+++ src/pages/contactsPage/contactsPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from './contactspage.module.css';
 import classNames from "classnames";
 
@@ -26,7 +25,7 @@ export const ContactsPage = () => {
 
         <div className={styles.right}>
           <div className={styles.contactsParagraph}>
-            <span>Вы можете заказать товары на сайте и забрать их бесплатно из наших пунктов самовывоза.</span>
+            <span className={styles.contactsParagraphSpan}>Вы можете заказать товары на сайте и забрать их бесплатно из наших пунктов самовывоза.</span>
             <span>У нас два пункта самовывоза:</span>
             <ul className={styles.list}>
               <li className={styles.listItem}><span className={styles.coloredText}>ПВЗ «Кокчетавский проспект»</span> (бесплатный самовывоз) </li>
@@ -35,9 +34,9 @@ export const ContactsPage = () => {
           </div>
           
           <div className={styles.infoParagraph}>
-            <p>ООО «Султан 124»</p>
-            <p>ИНН 9999999999</p>
-            <p>ОГРН 1111111111111</p>
+            <p className={styles.mainParagraph}>ООО «Султан 124»</p>
+            <p><b>ИНН</b> 9999999999</p>
+            <p><b>ОГРН</b> 1111111111111</p>
           </div>
         </div>
       </div>
